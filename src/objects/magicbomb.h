769ede653ef332186713fdd19e73207e371672d9
Nathan Misner <natemisner@msn.com>
2024-03-03 00:22:11 -0600

    added copyright lines to files

--- src/objects/magicbomb.h
+++ src/objects/magicbomb.h
@@ -1,20 +1,21 @@
 /* magicbomb.h: Magic bomb object code
-*
-*  This file is part of OpenMadoola.
-*
-*  OpenMadoola is free software: you can redistribute it and/or modify it
-*  under the terms of the GNU General Public License as published by the Free
-*  Software Foundation; either version 2 of the License, or (at your option)
-*  any later version.
-*
-*  OpenMadoola is distributed in the hope that it will be useful, but WITHOUT
-*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-*  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-*  more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with OpenMadoola. If not, see <https://www.gnu.org/licenses/>.
-*/
+ * Copyright (c) 2023 Nathan Misner
+ *
+ * This file is part of OpenMadoola.
+ *
+ * OpenMadoola is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * OpenMadoola is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenMadoola. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 #include "object.h"
