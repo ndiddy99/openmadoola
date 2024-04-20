058b24928565f219e225e2115f3ff68f8c9754dd
Nathan Misner <nathan@infochunk.com>
2024-04-20 12:39:45 -0500

    stopped relying on SDL2 header for integer types

--- src/constants.h
+++ src/constants.h
@@ -18,7 +18,14 @@
  */
 
 #pragma once
-#include <SDL.h>
+#include <stdint.h>
+
+typedef int8_t   Sint8;
+typedef uint8_t  Uint8;
+typedef int16_t  Sint16;
+typedef uint16_t Uint16;
+typedef int32_t  Sint32;
+typedef uint32_t Uint32;
 
 #define OPENMADOOLA_VERSION "1.0.3"
 
