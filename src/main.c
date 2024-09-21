8e89be42716a282f6af44597a4801f3b783c8a59
Nathan Misner <nathan@infochunk.com>
2024-09-21 09:03:57 -0500

    improved cli parameter validation

--- src/main.c
+++ src/main.c
@@ -24,6 +24,7 @@
 #include <Windows.h>
 #endif
 
+#include <stdio.h>
 #include <string.h>
 
 #include "demo.h"
@@ -33,6 +34,13 @@
 #include "task.h"
 #include "title.h"
 #include "weapon.h"
+
+static int checkFlag(char *str, char *match) {
+    if ((str[0] == '-') || (str[0] == '/')) {
+        return (strcmp(str + 1, match) == 0);
+    }
+    return 0;
+}
 
 int main(int argc, char **argv) {
     // Windows has two types of programs, "Console" and "Windows". Console
@@ -58,23 +66,47 @@ int main(int argc, char **argv) {
 
     if (!System_Init()) { return -1; }
 
-    if ((argc == 3) && (strcmp(argv[1], "-p") == 0)) {
+    if ((argc == 3) && checkFlag(argv[1], "p")) {
         SoundTest_RunStandaloneInit(argv[2]);
         Task_Init(SoundTest_RunStandaloneTask);
     }
-    else if ((argc == (8 + NUM_WEAPONS)) && (strcmp(argv[1], "-r") == 0)) {
+    else if ((argc == (8 + NUM_WEAPONS)) && checkFlag(argv[1], "r")) {
         int param = 2;
         char *filename = argv[param++];
         Uint8 type = (Uint8)atoi(argv[param++]);
-        Uint8 stage = (Uint8)atoi(argv[param++]) - 1;
+        if (type > GAME_TYPE_ARCADE) {
+            fprintf(stderr, "Game type must be 0 (original), 1 (plus) or 2 (arcade).\n");
+            return -1;
+        }
+        Uint8 stage = (Uint8)atoi(argv[param++]);
+        if ((stage < 1) || (stage > 16)) {
+            fprintf(stderr, "Stage must be between 1-16.\n");
+            return -1;
+        }
         Sint16 health = (Sint16)atoi(argv[param++]);
+        if ((health < 1) || (health > 5000)) {
+            fprintf(stderr, "Health must be between 1-5000.\n");
+            return -1;
+        }
         Sint16 magic = (Sint16)atoi(argv[param++]);
+        if ((magic < 1) || (magic > 5000)) {
+            fprintf(stderr, "Magic must be between 1-5000.\n");
+            return -1;
+        }
         Uint8 boots = (Uint8)atoi(argv[param++]);
+        if (boots > 3) {
+            fprintf(stderr, "Boots must be between 0-3.\n");
+            return -1;
+        }
         Uint8 weapons[NUM_WEAPONS];
         for (int i = 0; i < NUM_WEAPONS; i++) {
             weapons[i] = (Uint8)atoi(argv[param++]);
+            if (weapons[i] > 3) {
+                fprintf(stderr, "Weapon levels must be between 0-3.\n");
+                return -1;
+            }
         }
-        Game_RecordDemoInit(filename, type, stage, health, magic, boots, weapons);
+        Game_RecordDemoInit(filename, type, stage - 1, health, magic, boots, weapons);
         Task_Init(Game_RecordDemoTask);
     }
     else {
